aa28a297d2fb70a829bf7dff1ea6d2a070cf531b
Matheus Silva <matheus.mpbsilva@gmail.com>
2022-08-18 15:43:13 -0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+/********************** Menu ************************/
 const $menu = document.querySelector('#button');
 const $menuList = document.querySelector('.menu')
 
@@ -5,3 +6,9 @@ $menu.addEventListener('click', function(e){
     e.preventDefault();
     $menuList.classList.toggle('menu--open');
 })
+
+/********************** Scroll to top ************************/
+var btn = document.querySelector("#back-to-top");
+btn.addEventListener("click", function() {
+    window.scrollTo(0, 0);
+});
